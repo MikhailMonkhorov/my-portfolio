aeb56d4c2a4113beb862f906b270786a883e2f5d
MikhailMonkhorov <mikhailmonkhorov@gmail.com>
2025-09-09 18:26:38 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,11 @@ import "./Header.css";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleHomeClick = () => (window.location.href = "/");
-
   return (
     <header className="header">
       <div className="container">
-        <button onClick={handleHomeClick} className="logo">
-          <h2>Михаил Монхоров</h2>
-        </button>
+        <h2>Михаил Монхоров</h2>
+
         <nav className={`nav ${isMenuOpen ? "nav-open" : ""}`}>
           <a href="#about">Обо мне</a>
           <a href="#skills">Навыки</a>
